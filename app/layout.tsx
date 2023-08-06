c31b30b13c4d8805383db2451477f50168f5a54a
amitov <amitov.almuh@gmail.com>
2023-08-06 16:44:46 +0600

    analytics add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next'
 
 import { Toaster } from 'react-hot-toast'
-
+import { Analytics } from '@vercel/analytics/react';
 import '@/app/globals.css'
 import { fontMono, fontSans } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
